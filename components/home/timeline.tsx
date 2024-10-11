6a7f0d95048f37beb991232379ffb861a9eecd2e
Ulvin <ulvin.oguzlu@gmail.com>
2024-10-11 05:40:17 +0300

    Update timeline.tsx

--- components/home/timeline.tsx
+++ components/home/timeline.tsx
@@ -165,7 +165,7 @@ const TimelineSection = ({ isDesktop }: IDesktop) => {
 
     const titleSizeClass = size === ItemSize.LARGE ? "text-6xl" : "text-2xl";
     const logoString = image
-      ? `<img src='${image}' class='mb-2' loading='lazy' width='100' height='32' alt='${image}' />`
+      ? `<img src='${image}' class='h-8 mb-2' loading='lazy' width='100' height='32' alt='${image}' />`
       : "";
     const subtitleString = subtitle
       ? `<p class='text-xl mt-2 text-gray-200 font-medium tracking-wide'>${subtitle}</p>`
@@ -416,7 +416,7 @@ const TimelineSection = ({ isDesktop }: IDesktop) => {
         pin: true,
         pinSpacing: true,
       };
-      duration = timeline.totalDuration() / 6;
+      duration = timeline.totalDuration() / svgCheckpointItems.length;;
     } else {
       // Clearing out the right side on mobile devices
       screenContainer.current.innerHTML = "";
@@ -453,6 +453,7 @@ const TimelineSection = ({ isDesktop }: IDesktop) => {
     svgWidth,
     rightBranchX,
     screenContainer,
+    svgCheckpointItems.length,
     isDesktop,
     svgLength,
   ]);
